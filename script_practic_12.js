8784c702b778023ad98cdfb2cf7dcc7894b6918a
Pechtnov <tonichok@gmail.com>
2022-11-24 04:31:32 +0200

    end 12 practic

--- script_practic_12.js
+++ script_practic_12.js
@@ -8,4 +8,23 @@ function reverse(str) {
     return str.split('').reverse().join('');
 }
 
-console.log(reverse(someString))
+console.log(reverse(someString));
+
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY',];
+
+function availableCurr(arr, missingCurr) {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] === missingCurr) {
+            continue;
+        }
+        str += `${arr[i]}`;
+    }
+    console.log(str);
+    return str;
+}
+
+
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'USD');
